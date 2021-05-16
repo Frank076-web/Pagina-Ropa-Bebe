21734e796c522712542b91f4b4daa443c999c6a0
Frank076-web <franco.776@hotmail.com>
2021-05-16 15:43:58 -0300

    Agrego JavaScript para contenido dinamico mas sencillo

---
+++ app/app.js
@@ -0,0 +1,66 @@
+const imagenesZapatillitas = [
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas1.webp",
+    productosDescripcion: "Color: Rosa Bebe",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas2.webp",
+    productosDescripcion: "Color: Camufladas",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas3.webp",
+    productosDescripcion: "Color: Azul marino",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas4.webp",
+    productosDescripcion: "Color: Blanco con cerezas",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas5.webp",
+    productosDescripcion: "Color: Camufladas azules",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas6.webp",
+    productosDescripcion: "Color: Azules con flamencos",
+    precio: "Precio: $600",
+  },
+];
+const $header = document.getElementById("header");
+const $main = document.createElement("main");
+$main.classList.add("contenedor", "productosgrid");
+const $fragmentZapatillitas = document.createDocumentFragment();
+
+imagenesZapatillitas.forEach((el) => {
+  const $productoZapatillitas = document.createElement("div");
+
+  const $parrafo = document.createElement("p");
+  $parrafo.classList.add("productos__descripcion");
+  $parrafo.textContent = el.productosDescripcion;
+
+  const $cajaImagen = document.createElement("div");
+  $cajaImagen.classList.add("productos__imagen");
+  const $imagen = document.createElement("img");
+  $imagen.src = el.img;
+  $cajaImagen.appendChild($imagen);
+
+  const $precio = document.createElement("p");
+  $precio.classList.add("productos__precio");
+  const $spanPrecio = document.createElement("span");
+  $spanPrecio.classList.add("productos__precio-precio");
+  $spanPrecio.textContent = el.precio;
+  $precio.appendChild($spanPrecio);
+
+  $productoZapatillitas.appendChild($parrafo);
+  $productoZapatillitas.appendChild($cajaImagen);
+  $productoZapatillitas.appendChild($precio);
+
+  $fragmentZapatillitas.appendChild($productoZapatillitas);
+});
+
+$main.appendChild($fragmentZapatillitas);
+$header.insertAdjacentElement("afterend", $main);
