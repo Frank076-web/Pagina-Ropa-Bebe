53a88f7b75ea2ce815340977a43a69101051d34b
Frank076-web <franco.776@hotmail.com>
2021-05-16 16:12:56 -0300

    Añado imagenes a producto zapatillitas

--- app/zapatillitasnocaminantes.js
+++ app/zapatillitasnocaminantes.js
@@ -29,6 +29,26 @@ const $imagenesProductos = [
     productosDescripcion: "Color: Azules con flamencos",
     precio: "Precio: $600",
   },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas7.webp",
+    productosDescripcion: "Color: Negras",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas8.webp",
+    productosDescripcion: "Color: Blancas",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas9.webp",
+    productosDescripcion: "Color: Rojas",
+    precio: "Precio: $600",
+  },
+  {
+    img: "../Imagenes/Imagenes-de-conjuntos/zapatillitas-no-caminantes/zapatillitas10.webp",
+    productosDescripcion: "Color: Rosas",
+    precio: "Precio: $600",
+  },
 ];
 const $header = document.getElementById("header");
 const $main = document.createElement("main");
@@ -36,31 +56,32 @@ $main.classList.add("contenedor", "productosgrid");
 const $fragmentProductos = document.createDocumentFragment();
 
 $imagenesProductos.forEach((el) => {
+  // creo el div que contiene a los elementos del producto
   const $productoZapatillitas = document.createElement("div");
-
+  // primer parrafo
   const $parrafo = document.createElement("p");
   $parrafo.classList.add("productos__descripcion");
   $parrafo.textContent = el.productosDescripcion;
-
+  // contenedor de imagen e imagen
   const $cajaImagen = document.createElement("div");
   $cajaImagen.classList.add("productos__imagen");
   const $imagen = document.createElement("img");
   $imagen.src = el.img;
   $cajaImagen.appendChild($imagen);
-
+  // parrafo con el precio
   const $precio = document.createElement("p");
   $precio.classList.add("productos__precio");
   const $spanPrecio = document.createElement("span");
   $spanPrecio.classList.add("productos__precio-precio");
   $spanPrecio.textContent = el.precio;
   $precio.appendChild($spanPrecio);
-
+  // inserto al contenedor de todos los elementos del producto todos sus elementos
   $productoZapatillitas.appendChild($parrafo);
   $productoZapatillitas.appendChild($cajaImagen);
   $productoZapatillitas.appendChild($precio);
-
+  // Inserto cada producto en el fragmento
   $fragmentProductos.appendChild($productoZapatillitas);
 });
-
+// inserto en HTML
 $main.appendChild($fragmentProductos);
 $header.insertAdjacentElement("afterend", $main);
